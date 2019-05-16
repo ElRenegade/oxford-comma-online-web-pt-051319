7581344211e7de1ff04cb6314c4e64fdef127f6d
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-16 16:12:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,11 @@
 def oxford_comma(array)
-  # if array.length < 2
-  #   array.join(",")
-  # elsif array.length <3
-  #   array.join(" and ")
-  # end
+  if array.length < 2
+    array.join(",")
+  elsif array.length < 3
+    array.join(" and ")
+  elsif array.length < 4
+    array.slice_after(2).map { |e| e.join "," }.join(" and ")
+  end
 
-  array.slice_after(3).map { |e| e.join "," }.join(" and ")
+  
 end
