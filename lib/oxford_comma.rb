4e3d54b37e0d12a0d388f16d1584fba7362b53a8
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-16 16:07:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
-  if array.length < 2
-    array.join(",")
-  elsif array.length <3
-    array.join(" and ")
-  end
+  # if array.length < 2
+  #   array.join(",")
+  # elsif array.length <3
+  #   array.join(" and ")
+  # end
+
+  array.to_sentence(last_word_connector: ' and ')
 end
